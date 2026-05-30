774475c0c69e9b2a40d2f4159a8134734225a206
Taliat69 <134264715+Taliat69@users.noreply.github.com>
2026-05-30 22:04:30 +0100

    Update config.py

--- dlt_project/config.py
+++ dlt_project/config.py
@@ -1,7 +1,16 @@
 import os
 
-lake_db_url = os.getenv("lake_db_url")
-warehouse_db_url = os.getenv("warehouse_db_url")
+LAKE_DB_HOST = os.getenv("LAKE_DB_HOST")
+LAKE_DB_PORT = os.getenv("LAKE_DB_PORT", "5432")
+LAKE_DB_NAME = os.getenv("LAKE_DB_NAME")
+LAKE_DB_USER = os.getenv("LAKE_DB_USER")
+LAKE_DB_PASSWORD = os.getenv("LAKE_DB_PASSWORD")
+
+WAREHOUSE_DB_HOST = os.getenv("WAREHOUSE_DB_HOST")
+WAREHOUSE_DB_PORT = os.getenv("WAREHOUSE_DB_PORT", "5432")
+WAREHOUSE_DB_NAME = os.getenv("WAREHOUSE_DB_NAME")
+WAREHOUSE_DB_USER = os.getenv("WAREHOUSE_DB_USER")
+WAREHOUSE_DB_PASSWORD = os.getenv("WAREHOUSE_DB_PASSWORD")
 
 raw_schema = "raw"
 
